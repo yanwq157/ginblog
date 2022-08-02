502117eedac65318bcf02db47f4d86b00417d962
yanwq157 <1351472805@qq.com>
2022-08-02 15:00:11 +0800

    1

---
+++ routes/router.go
@@ -0,0 +1,21 @@
+package routes
+
+import (
+	"ginblog/utils"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func InitRouter() {
+	gin.SetMode(utils.AppMode)
+	r := gin.Default()
+	router := r.Group("api/v1")
+	{
+		router.GET("hello", func(context *gin.Context) {
+			context.JSON(http.StatusOK, gin.H{
+				"msg": "ok",
+			})
+		})
+	}
+	r.Run(utils.HttpPort)
+}
---
+++ utils/setting.go
@@ -0,0 +1,41 @@
+package utils
+
+import (
+	"fmt"
+	"gopkg.in/ini.v1"
+)
+
+var (
+	AppMode  string
+	HttpPort string
+
+	Db         string
+	DbHost     string
+	DbPort     string
+	DbUser     string
+	DbPassword string
+	DbName     string
+)
+
+func init() {
+	file, err := ini.Load("config/config.ini")
+	if err != nil {
+		fmt.Println("config reload err", err)
+	}
+	LoadServer(file)
+	LoadDataBase(file)
+}
+
+func LoadServer(file *ini.File) {
+	AppMode = file.Section("server").Key("AppMode").MustString("debug")
+	HttpPort = file.Section("server").Key("HttpPort").MustString(":8080")
+}
+
+func LoadDataBase(file *ini.File) {
+	Db = file.Section("database").Key("Db").MustString("mysql")
+	DbHost = file.Section("database").Key("DbHost").MustString("localhost")
+	DbPort = file.Section("database").Key("DbPort").MustString("3306")
+	DbUser = file.Section("database").Key("DbUser").MustString("root")
+	DbPassword = file.Section("database").Key("DbPassword").MustString("123123")
+	DbName = file.Section("database").Key("DbName").MustString("ginblog")
+}
