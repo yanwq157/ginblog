6747cd3a1bc2c315bf55738018d6843a9cf2d4df
yanwq157 <1351472805@qq.com>
2022-08-12 12:17:50 +0800

    commmit

--- go.mod
+++ go.mod
@@ -7,9 +7,12 @@ require (
 	github.com/gin-gonic/gin v1.8.1
 	github.com/go-playground/validator/v10 v10.11.0 // indirect
 	github.com/goccy/go-json v0.9.10 // indirect
+	github.com/lestrrat-go/file-rotatelogs v2.4.0+incompatible // indirect
+	github.com/lestrrat-go/strftime v1.0.6 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/pelletier/go-toml/v2 v2.0.2 // indirect
 	github.com/qiniu/go-sdk/v7 v7.11.1
+	github.com/rifflock/lfshook v0.0.0-20180920164130-b9218ef580f5 // indirect
 	github.com/sirupsen/logrus v1.9.0 // indirect
 	golang.org/x/crypto v0.0.0-20220722155217-630584e8d5aa
 	golang.org/x/net v0.0.0-20220728211354-c7608f3a8462 // indirect
--- middleware/logger.go
+++ middleware/logger.go
@@ -3,28 +3,58 @@ package middleware
 import (
 	"fmt"
 	"github.com/gin-gonic/gin"
+	retalog "github.com/lestrrat-go/file-rotatelogs"
+	"github.com/rifflock/lfshook"
 	"github.com/sirupsen/logrus"
-	"math"
 	"os"
 	"time"
 )
 
 func Logger() gin.HandlerFunc {
-	filePath := "log/log.log"
-	scr, err := os.OpenFile(filePath, os.O_RDWR|os.O_CREATE, 0755)
+	filePath := "log/gin-blog"
+	//linkName := "letestlog.log"
+	scr, err := os.OpenFile(filePath, os.O_RDWR|os.O_CREATE, 0755) // 创建一个log日志文件
 	if err != nil {
 		fmt.Println("err:", err)
 	}
-	logger := logrus.New()
-	logger.Out = scr
+	//由于logrus本身并不提供写文件, 并且按照日期自动分割, 删除过期日志文件的功能.
+	//一般情况下大家都是使用github.com/rifflock/lfshook配合github.com/lestrrat-go/file-rotatelogs来实现相关的功能
+	logger := logrus.New()             //实例化
+	logger.Out = scr                   //设置log文件输出 输出到文件，不在控制台输出
+	logger.SetLevel(logrus.DebugLevel) //设置日志级别
+
+	writer, _ := retalog.New( //自动切割日志文件
+		filePath+"%Y%m%d.log",                    //文件名
+		retalog.WithMaxAge(7*24*time.Hour),       //保留时间
+		retalog.WithRotationTime(24*time.Minute), //多长时间分割一次
+		//retaLog.WithLinkName(linkName),		//软链接
+	)
+	lfsHook := lfshook.WriterMap{ //WriterMap是将日志级别映射到 io.Writer 的映射
+		logrus.DebugLevel: writer,
+		logrus.ErrorLevel: writer,
+		logrus.FatalLevel: writer,
+		logrus.PanicLevel: writer,
+		logrus.WarnLevel:  writer,
+		logrus.InfoLevel:  writer,
+		logrus.TraceLevel: writer,
+	}
+	//
+	Hook := lfshook.NewHook(lfsHook, &logrus.TextFormatter{
+		TimestampFormat: "2006-01-02 15:04:05",
+	})
+	//调用AddHook时, 将Hook加入到LevelHooks map中
+	//程序打印log, 会最终执行到Entry.log()
+	//Entry.log()会调用fireHooks()
+	//fireHooks又会调用LevelHooks Fire()函数, 该函数会遍历所有的Hook, 从而执行相应的Hook
+	logger.AddHook(Hook)
 	return func(c *gin.Context) {
 		startTime := time.Now()
 		//gin里面是洋葱模型中间件，先执行c.Next上面的内容，在执行下一个中间件，下一个中间件也是执行c.Next上面的内容，都执行完后返回到第一个中间件c.Next后面的内容
 		c.Next()
 		//一个时间段
-		stopTime := time.Since(startTime)
+		stopTime := time.Since(startTime).Milliseconds()
 		//开销时间为一个整数  向上取整 stopTime的纳秒  格式化输出字符串
-		spendTime := fmt.Sprintf("%d ms", int(math.Ceil(float64(stopTime.Nanoseconds()))/1000000.0))
+		spendTime := fmt.Sprintf("%d ms", stopTime)
 		//请求客户端hostname
 		hostName, err := os.Hostname()
 		if err != nil {
